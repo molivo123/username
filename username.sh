90874682d664a7fc290b9c7abdce11e7579fad29
Matthew Olivo <molivo@chapman.edu>
2018-11-13 20:38:15 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # username.sh
 # Matthew Olivo
-echo "Enter a username that begins with a lowercase letter, can have numbers and an underscore, and must be atleast 3 characters but no more than 12."
+echo "Enter a username that begins with a lowercase letter, has numbers and an underscore, and must be atleast 3 characters but no more than 12."
 read USERNAME
 while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
